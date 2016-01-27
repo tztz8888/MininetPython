a7adb4f46bc54241d054b3e5e7d7ea3890bde61c
tztz8888 <ecowen@126.com>
2016-01-27 00:50:06 -0500

    change1

--- CreateTopo.py
+++ CreateTopo.py
@@ -49,10 +49,17 @@ def testTopo():
     for i in range(0,8):
         for j in range(0,8):
             src, dst = net.hosts[i], net.hosts[j]
-            print "testing", src.name, "<->", dst.name,
-            bandwidth = net.iperf( [ src, dst ], seconds=10 )
-            print bandwidth
-            flush()
+            print "Now testing bandwidth between h%s and h%s" %i %j
+            result=src.cmd('iperf -u -s &')
+            pid = src.cmd('echo $!')
+            print result
+
+            print "Done running iperf on server, starting client now"
+            result1=dst.cmd('iperf -c 10.0.0.1 -u -b 10000000')
+            print result1
+
+    print "Shutting down the iperf server"
+    h1.cmd('kill -9 $pid')
 
     net.stop()
 
