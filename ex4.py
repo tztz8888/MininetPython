5bb6205db695f00b4b31f398d9e172b1db5a1797
tztz8888 <ecowen@126.com>
2016-03-31 22:51:44 -0400

    new

--- ex4.py
+++ ex4.py
@@ -1,38 +1,74 @@
 #!/usr/bin/python
 
 from mininet.topo import Topo
-from mininet.net import Mininet
 from mininet.node import CPULimitedHost
 from mininet.link import TCLink
+from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
+from mininet.cli import CLI
+import sys
+flush = sys.stdout.flush
 
-class SingleSwitchTopo(Topo):
-    "Single switch connected to n hosts."
-    def build(self, n=2):
-        switch = self.addSwitch('s1')
-        for h in range(n):
-            # Each host gets 50%/n of system CPU
-            host = self.addHost('h%s' % (h + 1),
-               cpu=.5/n)
-            # 10 Mbps, 5ms delay, 10% loss, 1000 packet queue
-            self.addLink(host, switch,
-               bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True)
+class CustomTopo(Topo):
+    "6 siwthes, 8 hosts"
+    def build(self,lossRate=1):
+        switch=[]
+        host=[]
+        for s in range(0,4):
+            switch.append( self.addSwitch('s%s' %(s+1)) )
+        for h in range(0,2):
+            host.append( self.addHost('h%s' %(h+1) ) )
 
-def perfTest():
-    "Create network and run simple performance test"
-    topo = SingleSwitchTopo(n=4)
-    net = Mininet(topo=topo, 
-                  host=CPULimitedHost, link=TCLink)
+        self.addLink(host[0],switch[0],bw=20, delay='50ms',loss=lossRate)
+
+        self.addLink(switch[0],switch[1],bw=20, delay='50ms',loss=lossRate)
+        self.addLink(switch[1],switch[2],bw=20, delay='50ms',loss=lossRate)
+        self.addLink(switch[2],switch[3],bw=20, delay='50ms',loss=lossRate)
+
+        self.addLink(host[1],switch[3],bw=20, delay='50ms',loss=lossRate)
+
+def testTopo():
+    "Create network and run simple performance tests"
+    topo= CustomTopo(lossRate=1)
+    net=Mininet(topo=topo,host=CPULimitedHost, link=TCLink)
     net.start()
     print "Dumping host connections"
     dumpNodeConnections(net.hosts)
-    
-    print "Testing bandwidth between h1 and h4"
-    h1, h4 = net.get('h1', 'h4')
-    net.iperf((h1, h4))
+
+    print "Testing connectivity"
+    #Connectivity test by sending 10 ping messages between each other
+    for i in range(0,10):
+       net.pingAll()
+
+    print "test TCP bandwidth"
+    for i in range(0,8):
+        for j in range(0,8):
+            if i<j:
+                src, dst = net.hosts[i], net.hosts[j]
+                src.cmd( 'telnet', dst.IP(), '5001' )
+                print "testing", src.name, "<->", dst.name,
+                bandwidth = net.iperf( [ src, dst ], seconds=10 )
+                print bandwidth
+                flush()
+
+    print "test UDP loss rate at bandwidth 15 Mbps"
+    for i in range(0,8):
+        src=net.hosts[i]
+        result=src.cmd('iperf -u -s &')
+        pid = src.cmd('echo $!')
+        for j in range(0,8):
+            dst = net.hosts[j]
+            if i!=j:
+                print "client",dst.name,"-> server", src.name 
+                ipsrc= src.IP()
+                result1=dst.cmd('iperf -c %s -u -b 15M' %ipsrc)
+                print result1
+        print "Shutting down the iperf server\n"
+        src.cmd('kill -9 $pid')
+
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')
-    perfTest()
+    testTopo()
