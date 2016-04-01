0defc992d2ceecbe6a8fa0340ae75374873187be
tztz8888 <ecowen@126.com>
2016-03-31 22:56:56 -0400

    new

--- ex4.py
+++ ex4.py
@@ -30,7 +30,7 @@ class CustomTopo(Topo):
 
 def testTopo():
     "Create network and run simple performance tests"
-    topo= CustomTopo(lossRate=1)
+    topo= CustomTopo(lossRate=5)
     net=Mininet(topo=topo,host=CPULimitedHost, link=TCLink)
     net.start()
     print "Dumping host connections"
